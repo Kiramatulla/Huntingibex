a2e36a2c8f528bc0fba5c896538ba47936de454e
Kiramatulla <gulmitshatuber12@gmail.com>
2024-05-04 19:37:34 +0500

    team member section changed

--- src/app/components/widget/teammem.tsx
+++ src/app/components/widget/teammem.tsx
@@ -1,27 +1,42 @@
 import React from "react";
 import Image from "next/image";
-import kirama from "../../../../public/images/kiramatt.jpeg";
+import kiramat from "../../../../public/images/kiramatt.jpeg";
 import hassan from "../../../../public/images/hassan.jpg";
-import nahid from "../../../../public/images/karimalishah.jpg";
+import karim from "../../../../public/images/karimalishah.jpg";
 import Shared from "../widget/shared";
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export default function Teammem() {
   return (
-    <section className="mt-10 md:mt-20 mb-20 bg-gray-100 overflow-hidden">
-      <h1 className="text-center text-3xl md:text-4xl font-bold font-serif">OUR FABULOUS TEAM</h1>
-      <p className="text-center text-sm md:text-base font-bold font-serif m-3">MEET OUR FABULOUS TEAM OF EXPERT GUIDES AND HUNTERS</p>
-      <main className="flex flex-col md:flex-row md:justify-center md:space-x-8">
-        {/* Team Member 1 */}
-        <div className="m-4 md:m-8 flex flex-col items-center">
-          <div className="w-64 md:w-72 h-72 md:h-96 relative rounded-xl overflow-hidden">
-            <Image
-              src={kirama}
+    <div className=" h-screen lg:mb-20">
+    <div className="py-10 max-w-screen-lg mx-auto">
+        <div className="text-center mb-16">
+            <p className="mt-4 text-2xl leading-7  text-gray-900 font-extrabold">
+            OUR FABULOUS TEAM
+            </p>
+            <h3 className="text-xl sm:text-xl leading-normal font-bold tracking-tight text-gray-500">
+            MEET OUR FABULOUS TEAM OF PROFESSIONAL HUNTERS
+            </h3>
+        </div>
+
+
+        <div className="grid grid-cols-3 col-gap-10 ">
+
+            <div className="text-center bg-white lg:rounded-2xl lg:shadow-2xl">
+          <div className="lg:flex lg:justify-center">
+          <Image
+              src={kiramat}
               alt="huntimage"
-              className="absolute inset-0 w-full h-full object-cover"
+              width={300}
+              height={300}
+              className="lg:rounded-2xl"
+              
+              
+             
             />
           </div>
-          <div className="flex flex-row pt-6">
+           
+            <div className="flex flex-row pt-6 justify-center">
             <Shared url="https://web.facebook.com/kiramat.ullah.1048">
               <FaFacebook />
             </Shared>
@@ -32,56 +47,89 @@ export default function Teammem() {
               <FaYoutube />
             </Shared>
           </div>
-          <h2 className="mt-4 md:mt-6 font-semibold text-center">Kiramat Saleem</h2>
-          <h3 className="mt-1 md:mt-3 text-center">Gulmit Gojal Hunza</h3>
-        </div>
-        {/* Team Member 2 */}
-        <div className="m-4 md:m-8 flex flex-col items-center">
-          <div className="w-64 md:w-72 h-72 md:h-96 relative rounded-xl overflow-hidden">
+                <div className="p-4">
+                    <div className="text-md">
+                        <a href="#"
+                            className="hover:text-indigo-500 text-gray-900 font-semibold transition duration-500 ease-in-out">
+                              KIRAMAT ULLAH</a>
+                        <p className="text-gray-500 uppercase text-sm">FOUNDER</p>
+                    </div>
+                   
+                    
+                </div>
+
+            </div>
+            <div className="text-center bg-white lg:rounded-2xl lg:shadow-2xl ">
+            <div className="lg:flex lg:justify-center">
             <Image
               src={hassan}
               alt="huntimage"
-              className="absolute inset-0 w-full h-full object-cover"
+              width={300}
+              height={300}
+              className="lg:rounded-2xl"
+             
             />
-          </div>
-          <div className="flex flex-row pt-6">
-            <Shared url="www.facebook.com">
+            </div>
+            <div className="flex flex-row pt-6 justify-center">
+            <Shared url="https://web.facebook.com/kiramat.ullah.1048">
               <FaFacebook />
             </Shared>
-            <Shared url="www.instagram.com">
+            <Shared url="https://www.instagram.com/kiramat__/">
               <FaInstagram />
             </Shared>
             <Shared url="www.facebook.com">
               <FaYoutube />
             </Shared>
           </div>
-          <h2 className="mt-4 md:mt-6 font-semibold text-center">Hassan Aziz</h2>
-          <h3 className="mt-1 md:mt-3 text-center">Gulmit Gojal Hunza</h3>
-        </div>
-        {/* Team Member 3 */}
-        <div className="m-4 md:m-8 flex flex-col items-center">
-          <div className="w-64 md:w-72 h-72 md:h-96 relative rounded-xl overflow-hidden">
+                <div className="p-4">
+                    <div className="text-md">
+                        <a href="#"
+                            className="hover:text-indigo-500 text-gray-900 font-semibold transition duration-500 ease-in-out">
+                            HASSAN AZIZ</a>
+                        <p className="text-gray-500 uppercase text-sm"> TEAM</p>
+                    </div>
+                    
+                   
+                </div>
+
+            </div>
+            <div className="text-center bg-white lg:rounded-2xl lg:shadow-2xl">
+            <div className="lg:flex lg:justify-center">
             <Image
-              src={nahid}
+              src={karim}
               alt="huntimage"
-              className="absolute inset-0 w-full h-full object-cover"
+              width={300}
+              height={300}
+             className="lg:rounded-2xl"
             />
-          </div>
-          <div className="flex flex-row pt-6">
-            <Shared url="www.facebook.com">
+            </div>
+            <div className="flex flex-row pt-6 justify-center">
+            <Shared url="https://web.facebook.com/kiramat.ullah.1048">
               <FaFacebook />
             </Shared>
-            <Shared url="www.instagram.com">
+            <Shared url="https://www.instagram.com/kiramat__/">
               <FaInstagram />
             </Shared>
             <Shared url="www.facebook.com">
               <FaYoutube />
             </Shared>
           </div>
-          <h2 className="mt-4 md:mt-6 font-semibold text-center">Karim Ali Shah</h2>
-          <h3 className="mt-1 md:mt-3 text-center">Gulmit Gojal Hunza</h3>
+                <div className="p-4">
+                    <div className="text-md">
+                        <a href="#"
+                            className="hover:text-indigo-500 text-gray-900 font-semibold transition duration-500 ease-in-out">
+                            KARIM ALI SHAH</a>
+                        <p className="text-gray-500 uppercase text-sm"> TEAM</p>
+                    </div>
+                   
+                    
+                </div>
+
+            </div>
+
         </div>
-      </main>
-    </section>
+
+    </div>
+</div>
   );
 }
